106ff35bba8001ca9b27585b1b2541f4f244098d
Phil Hunt <phil@deviantlogic.co.uk>
2020-06-16 22:12:15 +0100

    Crude UI PoC using inline styles

--- src/main/markdowny/main.cljs
+++ src/main/markdowny/main.cljs
@@ -13,16 +13,23 @@
 
 (defn app []
   [:div
-   [:h1 "Hello, from Reagent!"]
-   [:textarea
-    {:on-change #(reset! markdown (-> % .-target .-value))
-     :value @markdown}]
-
-   [:div {:dangerouslySetInnerHTML {:__html (md->html @markdown)}}]
-   [:div (md->html @markdown)]])
-
-
-
+   [:h1 "Markdowny - A simple Markdown editor"]
+   [:div
+    {:style {:display :flex}}
+    [:div
+     {:style {:flex "1"}}
+     [:h2 "Markdown"]
+     [:textarea
+      {:on-change #(reset! markdown (-> % .-target .-value))
+       :value @markdown
+       :style {:resize "none"
+               :height "400px"
+               :width "100%"}}]]
+    [:div
+     {:style {:flex "1"
+              :padding-left "2em"}}
+     [:h2 "HTML preview"]
+     [:div {:dangerouslySetInnerHTML {:__html (md->html @markdown)}}]]]])
 
 (defn mount! []
   (rd/render [app]
