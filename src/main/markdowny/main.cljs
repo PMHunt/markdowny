4d4e0d5cfb903da4de4b8e8e1abbd1d7577da5e9
Phil Hunt <phil@deviantlogic.co.uk>
2020-06-16 21:09:51 +0100

    basic markdown editor works (dangerously)

--- src/main/markdowny/main.cljs
+++ src/main/markdowny/main.cljs
@@ -1,7 +1,37 @@
-(ns markdowny.main)
+(ns markdowny.main
+  (:require
+   [reagent.core :as r]
+   [reagent.dom :as rd]
+   ["showdown" :as showdown]))
+
+(defonce markdown (r/atom ""))
+
+(defonce showdown-converter (showdown/Converter.))
+
+(defn md->html [md]
+  (.makeHtml showdown-converter md))
+
+(defn app []
+  [:div
+   [:h1 "Hello, from Reagent!"]
+   [:textarea
+    {:on-change #(reset! markdown (-> % .-target .-value))
+     :value @markdown}]
+
+   [:div {:dangerouslySetInnerHTML {:__html (md->html @markdown)}}]
+   [:div (md->html @markdown)]])
+
+
+
+
+(defn mount! []
+  (rd/render [app]
+            (.getElementById js/document "app")))
 
 (defn main! []
-  (println "Hi! I am Main"))
+  (println "Hi! I am Main")
+  (mount!))
 
 (defn reload! []
-  (println "Reloaded, yay!"))
+  (println "Reloaded, yay!")
+  (mount!))
