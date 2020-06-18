9903a0069c5bb417c204b2f0db45f7e33de83192
Phil Hunt <phil@deviantlogic.co.uk>
2020-06-18 11:21:27 +0100

    copy to clipboard works, but hacky

--- src/main/markdowny/main.cljs
+++ src/main/markdowny/main.cljs
@@ -11,6 +11,27 @@
 (defn md->html [md]
   (.makeHtml showdown-converter md))
 
+;; copy-to-clipboard is port of:
+;; https://hackernoon.com/copying-text-to-clipboard-with-javascript-df4d4988697f
+
+(defn copy-to-clipboard [s]
+  (let [el (.createElement js/document "textarea")
+        selected (when (pos? (-> js/document .getSelection .-rangeCount))
+                   (-> js/document .getSelection (.getRangeAt 0)))]
+    (set! (.-value el) s)
+    (println "after el is set" (.-value el))
+    (.setAttribute el "readonly" "")
+    (set! (-> el .-style .-position) "absolute")
+    (set! (-> el .-style .-left) "-9999px")
+    (-> js/document .-body (.appendChild el))
+    (.select el)
+    (.execCommand js/document "copy")
+    (println "after copy to clipboard")
+    (-> js/document .-body (.removeChild el))
+    (when selected
+      (-> js/document .getSelection .removeAllRanges)
+      (-> js/document .getSelection (.addRange selected)))))
+
 (defn app []
   [:div.app
    [:h1 "Markdowny - A simple Markdown editor"]
@@ -19,7 +40,10 @@
      [:h2 "Markdown"]
      [:textarea.mdtext
       {:on-change #(reset! markdown (-> % .-target .-value)) ; swap! no work
-       :value @markdown}]]
+       :value @markdown}]
+     [:button
+      {:on-click #(copy-to-clipboard @markdown)}
+      "Copy to clipboard"]]
     [:div.htmlwindow
      [:h2 "HTML preview"]
      [:div {:dangerouslySetInnerHTML {:__html (md->html @markdown)}}]]]])
