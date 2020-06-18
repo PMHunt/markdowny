7bd51f260c7ea017f7a5842c5142ea202baf3481
Phil Hunt <phil@deviantlogic.co.uk>
2020-06-18 18:51:42 +0100

    bi-directional editing

--- src/main/markdowny/main.cljs
+++ src/main/markdowny/main.cljs
@@ -4,30 +4,49 @@
    [reagent.dom :as rd]
    ["showdown" :as showdown]))
 
-(defonce markdown (r/atom ""))
+; (defonce markdown (r/atom ""))
+; (defonce html (r/atom ""))
 
 (defonce showdown-converter (showdown/Converter.))
 
 (defn md->html [md]
   (.makeHtml showdown-converter md))
 
+(defn html->md [html]
+  (.makeMarkdown showdown-converter html))
+
+;;; bettter way
+(defonce text-state (r/atom {:format :md
+                             :value ""}))
+
+(defn ->md [{:keys [format value]}]
+  (case format
+    :md value
+    :html (html->md value)))
+
+
+(defn ->html [{:keys [format value]}]
+  (case format
+    :html value
+    :md (md->html value)))
+
 ;; copy-to-clipboard is port of:
 ;; https://hackernoon.com/copying-text-to-clipboard-with-javascript-df4d4988697f
 
 (defn copy-to-clipboard [s]
-  (let [el (.createElement js/document "textarea") ; create <textarea> element
+  (let [el (.createElement js/document "textarea") ; create temp. working area in DOM
         selected (when (pos? (-> js/document .getSelection .-rangeCount)) ; existing selection?
                    (-> js/document .getSelection (.getRangeAt 0)))] ; store selection
     (set! (.-value el) s) ; put string param into our new <textarea> element
  ;   (println "after el is set" (.-value el))
-    (.setAttribute el "readonly" "") ; readonly so people can't mess with it
+    (.setAttribute el "readonly" "") ; make `el`readonly, so can't mess with it
     (set! (-> el .-style .-position) "absolute")
     (set! (-> el .-style .-left) "-9999px") ; banish it to outer darkness
     (-> js/document .-body (.appendChild el))
     (.select el) ; select whatever is in the text area
     (.execCommand js/document "copy") ; copy selection, only works in user event
   ;  (println "after copy to clipboard")
-    (-> js/document .-body (.removeChild el))
+    (-> js/document .-body (.removeChild el)) ; get rid of temporary element `el`
     (when selected ; restore any original selection
       (-> js/document .getSelection .removeAllRanges)
       (-> js/document .getSelection (.addRange selected)))))
@@ -39,17 +58,28 @@
     [:div.mdwindow
      [:h2 "Markdown"]
      [:textarea.mdtext
-      {:on-change #(reset! markdown (-> % .-target .-value)) ; swap! no work
-       :value @markdown}]
+      {:on-change (fn [e]  ; reset! ok in JS, and swap! doesn't work
+                    (reset! text-state {:format :md
+                                        :value (-> e .-target .-value)})  )
+       :value (->md @text-state)}]
      [:button.copybtn
-      {:on-click #(copy-to-clipboard @markdown)}
+      {:on-click #(copy-to-clipboard (->md @text-state))}
       "Copy Markdown"]]
+
     [:div.htmlwindow
-     [:h2 "HTML preview"]
-     [:div {:dangerouslySetInnerHTML {:__html (md->html @markdown)}}]
+     [:h2 "HTML"]
+     [:textarea.mdtext
+      {:on-change (fn [e]
+                    (reset! text-state {:format :html
+                                        :value (-> e .-target .-value)}))
+       :value (->html @text-state)}]
      [:button.copybtn
-      {:on-click #(copy-to-clipboard (md->html @markdown))}
-      "Copy HTML"]]]])
+      {:on-click #(copy-to-clipboard (->html @text-state))}
+      "Copy HTML"]]
+
+    [:div.previewwindow
+     [:h2 "HTML Preview"]
+     [:div {:dangerouslySetInnerHTML {:__html (->html @text-state)}}]]]])
 
 (defn mount! []
   (rd/render [app]
